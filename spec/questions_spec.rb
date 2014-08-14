d7f7f0ef8b84f7eebdc0275bf1ebb1d2ca15c14f
gomenasaai <contramundee@gmail.com>
2014-08-14 18:56:26 +0100

    passed 2 more tests

--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -38,15 +38,15 @@ describe 'the Friday test :)' do
     expect(sorted).to eq [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
   end
 
-  # specify 'all_elements_except_first_3' do
-  #   n = all_elements_except_first_3 [1, 2, 3, 4, 5, 6, 7]
-  #   expect(n).to eq [4, 5, 6, 7]
-  # end
+  specify 'all_elements_except_first_3' do
+    n = all_elements_except_first_3 [1, 2, 3, 4, 5, 6, 7]
+    expect(n).to eq [4, 5, 6, 7]
+  end
 
-  # specify 'add_element_to_beginning_of_array' do
-  #   n = add_element_to_beginning_of_array [2, 3, 4, 5], 1
-  #   expect(n).to eq [1, 2, 3, 4, 5]
-  # end
+  specify 'add_element_to_beginning_of_array' do
+    n = add_element_to_beginning_of_array [2, 3, 4, 5], 1
+    expect(n).to eq [1, 2, 3, 4, 5]
+  end
 
   # specify 'array_sort_by_last_letter_of_word' do
   #   n = array_sort_by_last_letter_of_word ['sky', 'puma', 'maker']
