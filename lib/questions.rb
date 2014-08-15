57ecc5dc11ce7e11789bb9fc9f5f759f87facbf2
gomenasaai <contramundee@gmail.com>
2014-08-15 13:45:21 +0100

    passed 3 more tests

--- lib/questions.rb
+++ lib/questions.rb
@@ -47,17 +47,21 @@ end
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+	array.sort_by{|el| el[-1]}
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+	letter_count = (string.length/2.0).ceil
+	string[0, letter_count]
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+	-number.abs
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of 
@@ -65,6 +69,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+	array.partition(&:even?)
 end
 
 # count the numbers of elements in an element which are palindromes
--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -48,31 +48,31 @@ describe 'the Friday test :)' do
     expect(n).to eq [1, 2, 3, 4, 5]
   end
 
-  # specify 'array_sort_by_last_letter_of_word' do
-  #   n = array_sort_by_last_letter_of_word ['sky', 'puma', 'maker']
-  #   expect(n).to eq ['puma', 'maker', 'sky']
-  # end
+  specify 'array_sort_by_last_letter_of_word' do
+    n = array_sort_by_last_letter_of_word ['sky', 'puma', 'maker']
+    expect(n).to eq ['puma', 'maker', 'sky']
+  end
 
-  # specify 'get_first_half_of_string' do
-  #   a = get_first_half_of_string 'banana'
-  #   b = get_first_half_of_string 'apple'
+  specify 'get_first_half_of_string' do
+    a = get_first_half_of_string 'banana'
+    b = get_first_half_of_string 'apple'
 
-  #   expect(a).to eq 'ban'
-  #   expect(b).to eq 'app'
-  # end
+    expect(a).to eq 'ban'
+    expect(b).to eq 'app'
+  end
 
-  # specify 'make_numbers_negative' do
-  #   a = make_numbers_negative 5
-  #   b = make_numbers_negative -7
+  specify 'make_numbers_negative' do
+    a = make_numbers_negative 5
+    b = make_numbers_negative -7
 
-  #   expect(a).to eq -5
-  #   expect(b).to eq -7
-  # end
+    expect(a).to eq -5
+    expect(b).to eq -7
+  end
 
-  # specify 'separate_array_into_even_and_odd_numbers' do
-  #   n = separate_array_into_even_and_odd_numbers [1, 2, 3, 4, 5, 6, 7]
-  #   expect(n).to eq [[2, 4, 6], [1, 3, 5, 7]]
-  # end
+  specify 'separate_array_into_even_and_odd_numbers' do
+    n = separate_array_into_even_and_odd_numbers [1, 2, 3, 4, 5, 6, 7]
+    expect(n).to eq [[2, 4, 6], [1, 3, 5, 7]]
+  end
 
   # specify 'number_of_elements_that_are_palindromes' do
   #   n = number_of_elements_that_are_palindromes ['bob', 'radar', 'alex', 'noon', 'banana']
