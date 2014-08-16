3d5c5626e409febd57cae44878acefb94d22d332
gomenasaai <contramundee@gmail.com>
2014-08-16 19:56:28 +0100

    passed 5 more tests

--- lib/questions.rb
+++ lib/questions.rb
@@ -195,24 +195,29 @@ end
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+	string =~ /\W/ ? true : false
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+	range.max
 end
 
 # should return true for a 3 dot range like 1...20, false for a 
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+	range.exclude_end?
 end
 
 # get the square root of a number
 def square_root_of(number)
+	Math.sqrt number
 end
 
 # count the number of words in a file
 def word_count_a_file(file_path)
+	IO.read(file_path).split.length
 end
 
 # --- tougher ones ---
--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -164,38 +164,38 @@ describe 'the Friday test :)' do
     expect(n).to eq 'The Lion the Witch and the Wardrobe'
   end
 
-  # specify 'check_a_string_for_special_characters' do
-  #   a = check_a_string_for_special_characters 'ABCdef123'
-  #   b = check_a_string_for_special_characters 'ABC@def123!'
+  specify 'check_a_string_for_special_characters' do
+    a = check_a_string_for_special_characters 'ABCdef123'
+    b = check_a_string_for_special_characters 'ABC@def123!'
 
-  #   expect(a).to be_false
-  #   expect(b).to be_true
-  # end
+    expect(a).to be false
+    expect(b).to be true
+  end
 
-  # specify 'get_upper_limit_of' do
-  #   n = get_upper_limit_of 1..20
-  #   expect(n).to eq 20
-  # end
+  specify 'get_upper_limit_of' do
+    n = get_upper_limit_of 1..20
+    expect(n).to eq 20
+  end
 
-  # specify 'is_a_3_dot_range?' do
-  #   a = is_a_3_dot_range? 1..20
-  #   b = is_a_3_dot_range? 1...20
+  specify 'is_a_3_dot_range?' do
+    a = is_a_3_dot_range? 1..20
+    b = is_a_3_dot_range? 1...20
 
-  #   expect(a).to be_false
-  #   expect(b).to be_true
-  # end
+    expect(a).to be false
+    expect(b).to be true
+  end
 
-  # specify 'square_root_of' do
-  #   a = square_root_of 9
-  #   b = square_root_of 3
-  #   expect(a).to eq 3.0
-  #   expect(b).to eq 1.7320508075688772
-  # end
+  specify 'square_root_of' do
+    a = square_root_of 9
+    b = square_root_of 3
+    expect(a).to eq 3.0
+    expect(b).to eq 1.7320508075688772
+  end
 
-  # specify 'word_count_a_file' do
-  #   n = word_count_a_file 'lorem.txt'
-  #   expect(n).to eq 70
-  # end
+  specify 'word_count_a_file' do
+    n = word_count_a_file 'lorem.txt'
+    expect(n).to eq 70
+  end
 
   # specify 'call_method_from_string' do
   #   expect { call_method_from_string('foobar') }.to raise_error(NameError)
